cc857dd52ef5c34e4c00ec812b25fc99c0610b61
ThakuraAnki <codeankit.thakur@gmail.com>
2025-11-29 15:31:30 +0530

    hellp

--- DAS5.cpp
+++ DAS5.cpp
@@ -1,17 +1,23 @@
 #include<iostream>
+#include<algorithm>
+#include<vector>
 using namespace std;
 
 int main(){
-    int n1,n2,n3;
-    cin>>n1>>n2>>n3;
-    int largest=0;
+    int a[]={10,9,3,5,4,11,7,8};
+    int n=8;
+    vector<int>v;
+    for (int i = 0; i < n; i++)
+    {
+       if(v.size()==0 or a[i]>v.back()){
+        v.push_back(a[i]);
+       }
+       else{
+        int pos=upper_bound(v.begin(),v.end(),a[i])-v.begin();
+        v[pos]=a[i];
 
+       }
+    }
+    cout<<v.size()<<endl;
     
-    if(n1>largest)largest=n1;
-    if (n2>largest)largest=n2;
-    if(n3>largest) largest=n3;
-     
-    
-   
-    cout<<largest;
 }
